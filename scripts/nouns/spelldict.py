0a5d13ca6bda07cce980e8adaa52d9693edc95bd
linuxscout <taha_zerrouki@hotmail.com>
2017-05-09 19:40:33 +0100

    Add tags generation

--- scripts/nouns/spelldict.py
+++ scripts/nouns/spelldict.py
@@ -97,16 +97,18 @@ class SpellDict(csvdict.CsvDict):
             if nspell.validate_tags(noun_tuple, affix_tags, procletic, encletic_nm, suffix_nm):
 
                 if nspell.is_compatible_proaffix_affix(noun_tuple, procletic, encletic, suffix):
-                    vocalized, semi_vocalized, segmented= nspell.vocalize(noun_tuple['vocalized'], procletic,  suffix, encletic)
+                    vocalized, semi_vocalized, segmented = nspell.vocalize(noun_tuple['vocalized'], procletic,  suffix, encletic)
                     if VERIFY_INPUT: 
                         print (u"\t".join([  segmented,  vocalized])).encode('utf8')
+                        tags = self.get_tags(noun_tuple, affix_tags) 
+                        print (u"\t".join([  araby.strip_tashkeel(vocalized),  noun_tuple['unvocalized'], tags])).encode('utf8')
                     nb += 1
                     listfields = segmented.split('-')
                     if len(listfields) == 4:
                         pref = listfields[0]
                         stem = listfields[1]
                         suff = listfields[2]
-                        enc = listfields[3]
+                        enc  = listfields[3]
                         prefix_table.append(pref)
                         suffix_table.append(suff+enc)
                         stem_table.append(stem)
@@ -130,7 +132,60 @@ class SpellDict(csvdict.CsvDict):
                 print (u"%s/%s%s%s po:noun %s"%(stem, stem_tag, preftag, u''.join(sorted(set(flags_table[stem][preftag]))), original)).encode('utf8')
         #print nb, len(set(prefix_table)), "pref", len(set(suffix_table)), "suf",   len(set(stem_table)), "stem", (u" ".join(set(stem_table))).encode('utf8')         
         return line
-    
+        
+    def get_tags(self, noun_tuple, affix_tags):
+        
+        tags = u"-".join(affix_tags);
+        prefix = ""
+        if u"عطف" in affix_tags:
+            prefix +="W"
+        else:
+            prefix +="-"
+        if u"جر" in affix_tags:
+            prefix +="B"
+        else:
+            prefix +="-"            
+        if u"تعريف" in affix_tags:
+            prefix +="L"
+        else:
+            prefix +="-"            
+        conjug = ""
+        if (u"مؤنث" in affix_tags
+        or u"مؤنث" in noun_tuple['gender']):
+            conjug +="F"
+        elif (u"مذكر" in affix_tags
+        or u"مذكر" in noun_tuple['gender']):
+            conjug +="M"
+        else:
+            conjug +="-"             
+        if (u"جمع" in affix_tags or u"جمع مؤنث سالم" in affix_tags 
+            or u"جمع مذكر سالم" in affix_tags
+            or u"جمع" in  noun_tuple['number']):
+            conjug +="3"
+        elif u"مثنى" in affix_tags or noun_tuple["number"] == "3":
+            conjug +="2"
+        else:
+            conjug +="1" 
+        
+        if u"منصوب" in affix_tags:
+            conjug +="A"
+        elif u"مرفوع" in affix_tags:
+            conjug +="U"            
+        elif u"مجرور" in affix_tags:
+            conjug +="Y"            
+        elif u"مبني" in affix_tags:
+            conjug +="B"            
+        else:
+            conjug +="-" 
+            
+        encletic = ""
+        if u"مضاف" in affix_tags:
+            encletic +="H"            
+        else:
+            encletic +="-"  
+                    
+        tags = "N" +'.' +noun_tuple['wordtype']+'.'+ prefix+";"+conjug +";"+ encletic +u"-".join(affix_tags);
+        return tags;
     
     def get_suffix_tag(self, suffix, encletic):
         """give the suffix tags"""
@@ -170,28 +225,28 @@ class SpellDict(csvdict.CsvDict):
         print  line.encode('utf8');
     
     def add_footer(self):
-                """close the data set, used for ending xml, or sql"""
-                # display affixes rules
-                pronoun_list = [u"ه", u"ها", u"هما", u"هم", u"هن", u'كما', u'كم', u'كن', u'نا']
-                for pref in snconst.COMP_PREFIX_LIST_MODEL:
-                    pref_tag = snconst.COMP_PREFIX_LIST_MODEL[pref]
-                    print "PFX  %s  Y   1"%pref_tag
-                    print ("PFX  %s  0   %s  ."%(pref_tag, pref)).encode('utf8')
-                for suffix in sorted(self.suffix_tag):
-                    suffix_tag = self.suffix_tag[suffix]
-                    suffix_letters = u''.join( self.suffix_tag[suffix].split('-'))
-                    nb_rules = 1
-                    if suffix.endswith(u"ك"):
-                        nb_rules = len(pronoun_list)+1
-                    print "SFX  %s  Y   %d"%(suffix_tag,nb_rules)
-                    print ("SFX  %s  0   %s  ."%(suffix_tag, suffix)).encode('utf8')  
-                    if suffix.endswith(u"ك"):
-                        for pronoun in pronoun_list:
-                            print ("SFX  %s  0   %s  ."%(suffix_tag, suffix[:-1]+pronoun)).encode('utf8')  
-                            
-                # print suffix table
-                print "self.suffix_tag={",
-                for s, t in self.suffix_tag.items():
-                    print ("u'%s':'%s',"%(s,t)).encode('utf8')
-                print "}"
-                return """"""
+        """close the data set, used for ending xml, or sql"""
+        # display affixes rules
+        pronoun_list = [u"ه", u"ها", u"هما", u"هم", u"هن", u'كما', u'كم', u'كن', u'نا']
+        for pref in snconst.COMP_PREFIX_LIST_MODEL:
+            pref_tag = snconst.COMP_PREFIX_LIST_MODEL[pref]
+            print "PFX  %s  Y   1"%pref_tag
+            print ("PFX  %s  0   %s  ."%(pref_tag, pref)).encode('utf8')
+        for suffix in sorted(self.suffix_tag):
+            suffix_tag = self.suffix_tag[suffix]
+            suffix_letters = u''.join( self.suffix_tag[suffix].split('-'))
+            nb_rules = 1
+            if suffix.endswith(u"ك"):
+                nb_rules = len(pronoun_list)+1
+            print "SFX  %s  Y   %d"%(suffix_tag,nb_rules)
+            print ("SFX  %s  0   %s  ."%(suffix_tag, suffix)).encode('utf8')  
+            if suffix.endswith(u"ك"):
+                for pronoun in pronoun_list:
+                    print ("SFX  %s  0   %s  ."%(suffix_tag, suffix[:-1]+pronoun)).encode('utf8')  
+                    
+        # print suffix table
+        print "self.suffix_tag={",
+        for s, t in self.suffix_tag.items():
+            print ("u'%s':'%s',"%(s,t)).encode('utf8')
+        print "}"
+        return """"""
